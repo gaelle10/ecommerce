0f24eb8cb90dae1f52c902b0b01d91df2312d93d
gaelle10 <105851625+gaelle10@users.noreply.github.com>
2022-07-01 11:51:35 +0200

    creation de l'entité produit et sa migration

---
+++ src/Entity/Produit.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProduitRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProduitRepository::class)]
+class Produit
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $nom;
+
+    #[ORM\Column(type: 'float')]
+    private $prix;
+
+    #[ORM\Column(type: 'datetime')]
+    private $dt_creation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getDtCreation(): ?\DateTimeInterface
+    {
+        return $this->dt_creation;
+    }
+
+    public function setDtCreation(\DateTimeInterface $dt_creation): self
+    {
+        $this->dt_creation = $dt_creation;
+
+        return $this;
+    }
+}
